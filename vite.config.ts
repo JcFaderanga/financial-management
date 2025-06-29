8230d31ad67fba49f1973c2ddc9a882cc62802f7
jcfaderanga <you@example.com>
2025-06-29 19:09:37 +0800

    Minor update

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from 'path';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
-  base: process.env.VITE_BASE_PATH || "/",
+  base: process.env.VITE_BASE_PATH || "/financial-managementgit",
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
