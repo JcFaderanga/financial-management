765aa615f55ee87e24f5f825fd67100b279ea450
jcfaderanga <you@example.com>
2025-07-17 19:27:21 +0800

    Modify: add if state to check if it is hidden to disable overflow/scroll

--- src/components/modal/CustomModal.tsx
+++ src/components/modal/CustomModal.tsx
@@ -2,25 +2,21 @@ import { ReactNode,useEffect, useState } from "react"
 import { FaXmark } from "react-icons/fa6";
 const CustomModal = ({children, hidden, onClick}: {children: ReactNode,hidden: boolean, onClick?: ()=> void}) => {
 
-  const [isHidden, setIsHidden]= useState<boolean>(true);
      useEffect(() => {
     // Disable scroll
-    document.body.style.overflow = "hidden";
-
+    if(hidden){
+        document.body.style.overflow = "hidden";
+    }
+  
     // Re-enable scroll on unmount
     return () => {
       document.body.style.overflow = "";
     };
-  }, []);
-
-  useEffect(()=>{
-    setIsHidden(!isHidden)
-  },[hidden])
-
+  }, [hidden]);
 
   return (
     <section
-        className={`${hidden ? 'fixed' : 'hidden'}
+        className={`${hidden ? 'fixed' : ' hidden'}
          w-full px-4 h-screen bg-[rgb(0,0,0,.3)] inset-0`}>
         <div className=" bg-white rounded mt-40 max-w-200 mx-auto">
             <div onClick={onClick} className="p-2 flex justify-end cursor-pointer ">
