1280544958e254ed889b0e8ea5203c09b062c63d
jcfaderanga <you@example.com>
2025-07-18 17:01:17 +0800

    temorarly remove trigger auto close modal

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -12,7 +12,7 @@ import CustomModal from "@/components/modal/CustomModal";
 //import { useUserStore } from '@/store/useUserStore'
 const Dashboard = () => {
 const {handleFetchAllSpendings} = useFetchAllSpending();
-const {setSpendItems, spendings} = useSpendings();
+const {setSpendItems} = useSpendings();
 // const [totalSpent, setTotalSpent] = useState<number | string>('')
 const [addItemModal, setAddItemModal] = useState<boolean>(false);
 //const { user } = useUserStore();
@@ -30,7 +30,7 @@ const [addItemModal, setAddItemModal] = useState<boolean>(false);
 
 useEffect(()=>{
   setAddItemModal(false)
-},[spendings])
+},[])
 //use for fetching overall total spendings
 // useEffect(()=>{
 //     const fetchTotal = async ()=>{
