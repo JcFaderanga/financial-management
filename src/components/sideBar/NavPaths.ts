a542618e75c902036beb492cc4130bb11f01b12b
jcfaderanga <you@example.com>
2025-08-05 13:53:28 +0800

    [Remove][Nav Child] temorarily

--- src/components/sideBar/NavPaths.ts
+++ src/components/sideBar/NavPaths.ts
@@ -11,18 +11,18 @@ export const navItems: NavType[] = [
         { id: "spedingCalendar", label: "Spending Analysis", path: "/dashboard/spending_analysis", category: "homePage" },
       ],
     },
-    {
-      id: "accounts",
-      label: "Accounts",
-      path: "/accounts",
-      category: "homePage",
-      children: [
-        { id: "bank", label: "Bank Accounts", path: "/accounts/banks", category: "homePage" },
-        { id: "socials", label: "Socials Account", path: "/accounts/socials", category: "homePage" },
-      ],
-    },
-    { id: "bills", label: "Bills", path: "/bills", category: "homePage" },
-    { id: "liabilities", label: "Liabilities", path: "/liabilities", category: "homePage" },
-    { id: "transactions", label: "Transactions", path: "/transaction", category: "reports" },
+    // {
+    //   id: "accounts",
+    //   label: "Accounts",
+    //   path: "/accounts",
+    //   category: "homePage",
+    //   children: [
+    //     { id: "bank", label: "Bank Accounts", path: "/accounts/banks", category: "homePage" },
+    //     { id: "socials", label: "Socials Account", path: "/accounts/socials", category: "homePage" },
+    //   ],
+    // },
+    // { id: "bills", label: "Bills", path: "/bills", category: "homePage" },
+    // { id: "liabilities", label: "Liabilities", path: "/liabilities", category: "homePage" },
+    // { id: "transactions", label: "Transactions", path: "/transaction", category: "reports" },
   ];
   
